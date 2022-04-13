cbdfd7ec44ae6f8f38e5170ffc5818a6f1165260
“ <willrr.payall.2021@gmail.com>
2022-04-13 10:37:49 -0400

    [ADD]folder basic module the Academy

---
+++ __manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Odoo Academy',
+
+    'summary' : """ Academy app to manage Training""",
+
+    'description': """ 
+    Academy Module ti manage Training
+    -Courses
+    -Sessions
+    -Attendees
+    """,
+
+    'autor': 'Payall',
+    'developer': 'Willber R.R',
+    'website': 'htps://www.odoo.com',
+
+    'category': 'Training',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+
+    ],
+
+    'demo': [
+
+    ],
+}
