dc9a09feec966f8bab7a1b20d0257d87f97e6fc8
“ <willrr.payall.2021@gmail.com>
2022-04-18 16:25:01 -0400

    [ADD]module course

--- models/course.py
+++ models/course.py
@@ -2,7 +2,19 @@
 
 from odoo import models, fields, api
 
-Class Course(models.Model):
+class Course(models.Model):
+
     _name = 'academy.course'
     _description = 'Course Info'
+
+    name = fields.Char(string='Title', required=True)
+    description = fields.Text(String='Descritpion')
+
+    level = fields.Selection(string='Level',
+                             selection=[('beginner', 'Beginner)'
+                                         'intermediate', 'Intermediate'),
+                                        ('advanced', 'Advanced')],
+                             copy= False)
+    activate = fields.Boolean(string='Activate', default=True)
+
 
